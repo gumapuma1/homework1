45a2788c029ddd5d87b1a241d749425b5569c3a1
gumapuma1 <gumapumaa@gmail.com>
2020-05-29 09:22:14 +0300

    Task 2

---
+++ week1_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+const double PI = 3.14159;
+
+int main() {
+    double r,s,p;
+
+    cout << "Enter circle radius\n";
+    cin >> r;
+
+    s=PI*r*r;
+    p=2*PI*r;
+
+    cout << "S = " << s << endl;
+    cout << "P = " << p << endl;
+
+    return 0; 
+}
